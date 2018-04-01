c5ef29be5d928fe93496c708c7fd32e2c708dbd9
Roger Filmyer <roger.filmyer@gmail.com>
2018-04-01 15:36:18 -0400

    added second graph

--- FlightData/app.R
+++ FlightData/app.R
@@ -37,17 +37,19 @@ ui <- fluidPage(
          selectizeInput("Origin",
                      "Select your origin:", 
                      sort(unique(flights$ORIGIN)),
-                     multiple = TRUE),
+                     multiple = TRUE,
+                     selected = c("MIA")),
          
          selectizeInput("Destination",
                         "Select your destination:", 
-                        sort(unique(flights$DEST)),
+                        NA,
                         multiple = TRUE)
       ),
       
       # Show a plot of the generated distribution
       mainPanel(
-         plotOutput("flightPlot")
+         plotOutput("totalFlightsPlot"),
+         plotOutput("cancelledPlot")
       )
    )
 )
@@ -58,23 +60,44 @@ server <- function(input, output, session) {
      update_destination_choices <- reactive(flights %>%
                                               filter(flights$ORIGIN %in% input$Origin) %>%
                                               group_by(DEST) %>%
-                                              summarize(count = n()))
+                                              summarize(count = n()) %>%
+                                              arrange(desc(count)))
   
      observe({updateSelectizeInput(session = session, 
                                    inputId ="Destination", 
-                                   choices = sort(update_destination_choices()$DEST))})
+                                   choices = sort(update_destination_choices()$DEST),
+                                   )})
      
      # use filtered_flights_by_carrier() instead of 
      # always copying/pasting the filtering in graphs
      
-     filtered_flights_by_carrier<- reactive(flights %>%
+     filtered_flights_by_carrier <- reactive(flights %>%
                                               filter(ORIGIN %in% input$Origin & 
                                                      DEST %in% input$Destination) %>%
                                               group_by(UNIQUE_CARRIER)
                                             )
      
+     # Create a single Vaporwave Theme
+     vaporwave_theme <- theme_bw() +
+       theme(panel.border = element_blank())
      
-     output$flightPlot <- renderPlot({ #Cancellation plot
+     # If you get "RHS" errors, add the command to the list like this one
+     vaporwave_theme <- list(vaporwave_theme, 
+                             scale_fill_manual(values=rep(aesthetic(name="crystalpepsi"), times=4)),
+                             guides(fill = FALSE))
+     
+     output$totalFlightsPlot <- renderPlot({
+       filtered_flights_by_carrier() %>%
+         summarise(num_flights = n()) %>%
+         ggplot(aes(x = UNIQUE_CARRIER, y = num_flights)) +
+         geom_bar(aes(fill = UNIQUE_CARRIER),stat = "identity") + 
+         xlab("Carriers") +
+         ylab("Number of scheduled flights") + 
+         ggtitle("Number of total flights by carrier, 2017") +
+         vaporwave_theme 
+     })
+     
+     output$cancelledPlot <- renderPlot({ #Cancellation plot
         filtered_flights_by_carrier() %>%
         summarise(pct_cancelled = mean(CANCELLED)) %>%
         ggplot(aes(x = UNIQUE_CARRIER, y = pct_cancelled)) +
@@ -83,12 +106,7 @@ server <- function(input, output, session) {
          ylab("Percent of flight cancelled") + 
          scale_y_continuous(labels = scales::percent) +
          ggtitle("Percent of flights cancelled by carrier") +
-         theme_bw() +
-         theme(panel.border = element_blank()) +
-         guides(fill = FALSE) +
-         scale_fill_manual(values=rep(aesthetic(name="crystalpepsi"), times=4)) 
-     
-     
+         vaporwave_theme 
    })
 }
 
